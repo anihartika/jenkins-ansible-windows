aee762f8eafa87c9b3fc89e44b282e616f4abc34
azureaws <131311126+azureaws@users.noreply.github.com>
2023-07-11 18:28:20 -0400

    Update ubuntu.tf

--- ubuntu.tf
+++ ubuntu.tf
@@ -1,74 +1,72 @@
 module "ec2_MCSLAPPUbuntu" {
   source = "terraform-aws-modules/ec2-instance/aws"
   version = "~>3.0"
-  user_data = data.template_file.userdata.rendered
+  user_data = data.template_file.userdata_ubuntu.rendered
 
-  ami = "ami-0d70546e43a941d70"//Replace these valuses
-  instance_type = "m6i.large"
-  key_name = "my-key-pair" //Replace these values
-  security_groups = ["my-security-group"]
 
-  user_data = data.template_file.userdata.rendered
+resource "aws_vpc" "example_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
 
-  tags = {
-    Name = "my-ec2-instance"
+resource "aws_subnet" "example_subnet" {
+  vpc_id                  = aws_vpc.example_vpc.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "us-east-1a"
+}
+
+resource "aws_security_group" "example_sg" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  iam_instance_profile = "my-iam-instance-profile"
-
-  vpc_id = aws_vpc.default.id
-  subnet_id = aws_subnet.default.id
-
-  # Attach the IAM role to the instance
-  iam_instance_profile_attachment {
-    instance_id = module.ec2_instance.id
-    iam_instance_profile_arn = aws_iam_role.default.arn
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-data "template_file" "userdata" {
-  template = file("userdata.sh")
+resource "aws_iam_instance_profile" "example_iam_instance_profile" {
+  name = "ubuntu-instance-profile"
+  role_arn = "arn:aws:iam::<account_id>:role/<role_name>"
 }
 
-resource "aws_vpc" "default" {
-  cidr_block = "10.0.0.0/16" //Replace these values
-}
+data "template_file" "user_data_ubuntu" {
+  template = <<EOF
+#!/bin/bash
 
-resource "aws_subnet" "default" {
-  vpc_id = aws_vpc.default.id
-  cidr_block = "10.0.0.0/24" //Replace these values
-}
-
-resource "aws_iam_role" "default" {
-  name = "my-iam-role"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow"
-    }
-  ]
-}
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
 EOF
-
-  policy {
-    name = "my-iam-role-policy"
-    policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "*",
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
 }
-EOF
+
+  ami = "ami-0123456789abcdef0"
+  instance_type = "m2i.large"
+  monitoring = true
+  vpc_id = aws_vpc.example.id //check for vpc section and modify it
+  subnet_id = aws_subnet.example_subnet.id //Replace the values
+  associated_public_ip_address=true
+  security_groups = [aws_security_group.example_sg.id] //replace the values
+  iam_instance_profile = aws_iam_instance_profile.example_iam_instance_profile.name //replace the values
+  key_name = ubuntu.pem
+
+
+tags = {
+    Name = "example-instance"
   }
 }
+
+
+
+
+
+
+
