e9fba01ce947124b0b592b73a844786f9485c2fa
azureaws <131311126+azureaws@users.noreply.github.com>
2023-08-22 16:01:13 -0400

    Update host.sh

--- host.sh
+++ host.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ansible-inventory --list|grep -o '"private_ip_address": "[^"]*'|awk -F': "' '{print $2}'|head -1 > hosts.txt
+ansible-inventory --list|grep -o '"private_ip_address": "[^"]*'|awk -F': "' '{print $2}'|sort|uniq > hosts.txt
 hosts_file="hosts.txt"
 inventory_file="inventory.ini"
 
